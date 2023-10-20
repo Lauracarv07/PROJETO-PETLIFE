5711512548354f0e061ff9863888932a181b4a8e
Lauracarv07 <145488140+Lauracarv07@users.noreply.github.com>
2023-10-20 07:28:43 -0300

    ATUALIZANDO

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+ var elementosDuvida = document.querySelectorAll('.duvida')
+
+ elementosDuvida.forEach(function (duvida) {
+    duvida.addEventListener('click', function (){
+        ducida.classList.toggle('ativa')
+
+    })
+
+ })
+
+
+
+
